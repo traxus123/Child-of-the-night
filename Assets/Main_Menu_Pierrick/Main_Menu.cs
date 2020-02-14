8a91c515c56046fd8f4f0a7611ffece029ad05b2
traxusfr <thomas.f.rebouillat@gmail.com>
2020-02-14 15:22:28 +0100

    Intégration du menu de Pierrick <3

---
+++ Assets/Main_Menu_Pierrick/Main_Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void Play_Pressed() 
+    {
+        SceneManager.LoadScene("SampleScene");//CHANGER LE NOM DE LA SCENE A CHARGER
+    }
+
+    public void Quit_Pressed()
+    {
+        Application.Quit();
+    }
+}
